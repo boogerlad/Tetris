69a3c3dee7f1950f6d2a81dc45ef63e5e32f9abe
boogerlad <boogerlad@gmail.com>
2013-08-03 23:10:54 -0400

    Screw spaces.

--- level2.cc
+++ level2.cc
@@ -3,24 +3,24 @@
 
 Block* Level2::makeBlock()
 {
-   int num = rand() % 7;
-   switch(num)
-   {
-      case 0:
-         return new Block('I', 2);
-      case 1:
-         return new Block('J', 2);
-      case 2:
-         return new Block('L', 2);
-      case 3:
-         return new O(2);
-      case 4:
-         return new Block('S', 2);
-      case 5:
-         return new Block('T', 2);
-      case 6:
-         return new Block('Z', 2);
-      default:
-         return 0;
-   }
+	int num = rand() % 7;
+	switch(num)
+	{
+		case 0:
+			return new Block('I', 2);
+		case 1:
+			return new Block('J', 2);
+		case 2:
+			return new Block('L', 2);
+		case 3:
+			return new O(2);
+		case 4:
+			return new Block('S', 2);
+		case 5:
+			return new Block('T', 2);
+		case 6:
+			return new Block('Z', 2);
+		default:
+			return 0;
+	}
 }
--- level3.cc
+++ level3.cc
@@ -3,26 +3,26 @@
 
 Block* Level3::makeBlock()
 {
-   int num = rand() % 9;
-   switch(num)
-   {
-      case 0:
-         return new Block('I', 3);
-      case 1:
-         return new Block('C', 3);
-      case 2:
-         return new Block('L', 3);
-      case 3:
-         return new O(3);
-      case 4:
-      case 5:
-         return new Block('S', 3);
-      case 6:
-      case 7:
-         return new Block('T', 3);
-      case 8:
-         return new Block('Z', 3);
-      default:
-         return 0;
+	int num = rand() % 9;
+	switch(num)
+	{
+		case 0:
+			return new Block('I', 3);
+		case 1:
+			return new Block('C', 3);
+		case 2:
+			return new Block('L', 3);
+		case 3:
+			return new O(3);
+		case 4:
+		case 5:
+			return new Block('S', 3);
+		case 6:
+		case 7:
+			return new Block('T', 3);
+		case 8:
+			return new Block('Z', 3);
+		default:
+			return 0;
    }
 }
