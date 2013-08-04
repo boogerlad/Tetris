a42a2f904f1b6ddd1221d14af869d05177ff470d
boogerlad <boogerlad@gmail.com>
2013-08-04 01:25:50 -0400

    fix level3

--- level3.cc
+++ level3.cc
@@ -9,7 +9,7 @@ Block* Level3::makeBlock()
 		case 0:
 			return new Block('I', 3);
 		case 1:
-			return new Block('C', 3);
+			return new Block('J', 3);
 		case 2:
 			return new Block('L', 3);
 		case 3:
@@ -18,8 +18,8 @@ Block* Level3::makeBlock()
 		case 5:
 			return new Block('S', 3);
 		case 6:
-		case 7:
 			return new Block('T', 3);
+		case 7:
 		case 8:
 			return new Block('Z', 3);
 		default:
