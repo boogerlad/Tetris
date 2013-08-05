ea4b55045581cfdf9d5cac3e509556357dd702f6
boogerlad <boogerlad@gmail.com>
2013-08-05 02:37:50 -0400

    build files in index, but deleted before push

---
+++ level1.d
@@ -0,0 +1,1 @@
+level1.o: level1.cc level1.h blockgenerator.h block.h o.h
---
+++ main.d
@@ -0,0 +1,2 @@
+main.o: main.cc grid.h scoreboard.h block.h level.h level0.h \
+ blockgenerator.h o.h level1.h level2.h level3.h
